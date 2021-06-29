cf50ed90e3c49b861ddd695e7cfdcbe14a92f6fb
MicroFish91 <mwfisher91@gmail.com>
2021-06-29 16:34:59 -0700

    added Apollo server

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+tsc tut:
+2) demo configuration inheritance
+
+
+
+
+
 npm init -y
 
 Typescript Config:
@@ -10,3 +17,6 @@ yarn add @mikro-orm/cli @mikro-orm/core @mikro-orm/migrations @mikro-orm/postgre
 
 after setting config, create migrations using:
 npx mikro-orm migration:create
+
+const post = orm.em.create(Post, { title: 'my first post' });
+await orm.em.persistAndFlush(post);
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
-import mikroConfig from './mikro-orm.config'
+import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+import 'reflect-metadata';
 
 const main = async () => {
-    const orm = MikroORM.init(mikroConfig);
-    await orm.getMigrator().up(); // Runs table migrations
+    const orm = await MikroORM.init(mikroConfig);
+    // await orm.getMigrator().up(); // Runs table migrations
 
-    const post = orm.em.create(Post, {title: 'my first post'});
-    await orm.em.persistAndFlush(post);
+    const app = express();
 
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em })
+    });
+
+    apolloServer.applyMiddleware({ app });
+
+    const port: string | number = process.env.PORT || 3000;
+
+    // localhost:3000/graphql
+    app.listen(port, () => {
+        console.log(`Server started on port ${port}`);
+    });
+
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
 }
 
 main();
